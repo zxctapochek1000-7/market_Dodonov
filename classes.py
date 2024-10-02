8ff6e75382b3513ff1c33ff19111819d9b47849c
zxctapochek1000-7 <165178897+zxctapochek1000-7@users.noreply.github.com>
2024-10-02 19:26:40 +0400

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,1 +1,26 @@
+class Product:
+    def __init__(self, name: str, cost: float):
+        self.name = name
+        self.cost = cost
 
+    def __str__(self):
+        return self.name + " по цене: " + str(self.cost) + "р."
+
+
+class CommandInput:
+    def __init__(self, command_str: str):
+        command_split = command_str.split(" ")
+
+        self.command = command_split[0]
+        self.params = command_split[1:]
+
+
+class Prorab:
+    def __init__(self, spisok: {}):
+        self.spisok = spisok
+
+
+class ProductStorage:
+    def __init__(self, name: str, storage: [Product]):
+        self.name = name
+        self.storage = storage
